6107af53f052279493e723a5bbf6a930ff1e8bf9
pepereyez <42728106+pepereyez@users.noreply.github.com>
2019-09-23 17:40:41 -0500

    Update postApi.py

--- postApi.py
+++ postApi.py
@@ -1,11 +1,1 @@
-import requests
-import json
-if __name__ == '__main__':
-    url='http://httpbin.org/post'
-    payload={'nombre':'adrian','curso':'pyhton','nivel':'expert'}
-
-    response=requests.post(url,data=payload)
-    print(response.url)
-
-    if response.status_code==200:
-        print(response.content)
+print("hola")
